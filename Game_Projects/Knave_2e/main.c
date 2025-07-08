92ba9bb9d65d99dd4a23b66157baf4fb265c862b
jaztitch <alterjaz@gmail.com>
2025-07-07 19:17:57 -0500

    Add files via upload

---
+++ Game_Projects/Knave_2e/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+char playerName[30];
+char playerDesire[10];
+int playerLevel = 0;
+int playerExperience = 0;
+int playerAbilityPoints = 0;
+int playerOccupation1 = 0;
+int playerOccupation2 = 0;
+char playerDescription[400] ="";
+
+int playerStr = 0;
+int playerDex = 0;
+int playerCon = 0;
+int playerInt = 0;
+int playerWis = 0;
+int playerCha = 0;
+
+    return 0;
+}
