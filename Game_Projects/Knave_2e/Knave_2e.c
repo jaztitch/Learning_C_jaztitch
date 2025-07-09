124974ad82912b857713c6f50af6237c82252b21
jaztitch <alterjaz@gmail.com>
2025-07-09 12:39:41 -0500

    updated to include a few more prompts
    Updated to include array from available genders: male, female, nonbinary
    Updated to include array of available first names: adam, alice, rowan
    
    Includes new variables "positiveReply" and "negativeReply" for response checking purposes using "if-else" statements.
    
    requests and sets player gender
    requests and sets player name
    if player states they do not know their name, game sets from an array based on random entry using srand and rand()
    
    unchanged: requests and sets player desire.
    
    Player desire intended to be hidden feature that works with hidden "Luck" attribute, affecting likeliness of receiving items or discovering areas related to "playerDesire".

--- Game_Projects/Knave_2e/Knave_2e.c
+++ Game_Projects/Knave_2e/Knave_2e.c
@@ -3,8 +3,17 @@
 
 int main(){
 	
+srand(time(NULL));
+int randomNum = rand() %3;
+	
+char *Names[] = {"Adam", "Alice", "Rowan"};
+	
 char *desHere[] = {"gold", "adventure", "food", "glory", "shelter", "safety", 0};
+char *genders[] = {"male", "female", "nonbinary", 0};
+char positiveReply[] = "yes";
+char negativeReply[] = "no";
 
+char playerGender[10]="";
 char playerName[30]="";
 char playerDesire[10]="";
 int playerLevel = 0;
@@ -23,9 +32,46 @@ int playerCha = 0;
 
 int i = 0;
 
-printf("\"Hello, new friend. What is your name?\"\nYou tell them your name: ");
-fgets(playerName, sizeof(playerName), stdin);
-playerName[strlen(playerName)-1]='\0';
+char reply[4]= "";
+
+printf("Please enter you gender. Options are: \n");
+for(i = 0; i < 3; i++) {
+	printf("%s ", genders[i]);
+}
+printf("\n");
+scanf("%s", &playerGender);
+while ((getchar()) != '\n');  
+i = 0;
+
+while(genders[i]){
+	if(strcmp(genders[i], playerGender) == 0){
+		printf("Then you shall be recognized as a %s.\n", playerGender);
+		break;
+	}
+	else{
+		printf("My apologies, I don\'t believe I recognize that gender.\n");
+		printf("Please enter a gender we recognize in these lands.\n");
+		scanf("%s", &playerGender);
+		while ((getchar()) != '\n');
+		break;
+	}	
+}
+
+	printf("Do you know your name?\n");
+	scanf("%s", &reply);
+while ((getchar()) != '\n');
+	
+	if (strcmp(reply, positiveReply) == 0){
+		printf("Please tell me your name so that we may proceed.\n");
+		fgets(playerName, sizeof(playerName), stdin);
+		playerName[strlen(playerName)-1]='\0';
+	}
+	else if (strcmp(reply, negativeReply) == 0){
+		strcpy(playerName, Names[randomNum]);
+		printf("Then you will be known as %s.\n", playerName);
+	}
+
+i = 0;
 
 printf("Welcome to the land of Teirthos, %s\n", playerName);
 printf("What brings you to these lands?\nYou state what you\'ve come to Teirthos for: ");
